56174e196c994cf95ac4daca0a2b6694c1f811d4
alnrfLO <raf.atns1103@gmail.com>
2026-02-05 15:18:26 +0100

    Add files via upload

--- js/script4.js
+++ js/script4.js
@@ -13,13 +13,6 @@ const text = {
     sectionIntroText: "Né au Portugal, je suis passionné par le développement web, à la fois front-end et back-end. Et non, tous les geeks ne passent pas *que* leur vie derrière un écran... parfois, on touche aussi de l’herbe (vraie ou en CSS) .",
     sectionSportTitle: "Sport & Karaté",
     sectionSportText: `J'adore énormément le sport, ce qui m’a permis d’avoir une carrière dans le karaté. Toute ma vie tourne autour de ce sport : compétitions, cours donnés à des enfants, coaching d’enfants… Voici quelques résultats marquants :\r\n- 2020 : Championnat de France — 3ème place\r\n- 2021 : Compétition internationale au Portugal — 1ère place\r\n- 2021 : Compétition internationale au Luxembourg — 1ère place\r\n- 2022 : Compétition internationale en Belgique — 1ère place`,
-    sectionCinemaTitle: "Cinéma",
-    sectionCinemaText: "Par la suite, je suis passionné par le cinéma. Je trouve que c’est un vrai bonheur d’aller au cinéma voir de bons films qui, en plus de nous enrichir culturellement, nous font prendre conscience de plein de choses.",
-    sectionEsportTitle: "Esport & Jeux vidéo",
-    sectionEsportText: "Les jeux vidéo ont aussi beaucoup compté pendant ma jeunesse, où j’étais concentré uniquement sur le sport. En rentrant chez moi, mes sources de motivation étaient les jeux. J’ai donc intégré des équipes Esport qui m’ont donné des expériences uniques. Je pense qu'avoir participé à des compétitions de Karaté et à des équipes d'Esport m'a permis d’améliorer mes soft skills, notamment la communication, le travail d'équipe et la gestion du stress.",
-    sectionCuisineTitle: "Cuisine",
-    sectionCuisineText: "Ce que j'aime surtout dans la cuisine, c'est l'aspect créatif, de pouvoir combiner différents ingrédients et épices pour arriver à un plat unique. C'est un peu comme le développement web, où l'on combine différents éléments pour arriver à un site web fonctionnel et agréable à utiliser.",
-    sectionMotivationText: "Mon portfolio est le fruit de la créativité, mais surtout du travail acharné. Je suis toujours à la recherche d'apprendre, d'affronter de nouveaux défis et de créer des expériences web modernes et agréables pour ceux qui visitent mes sites.",
     cvButtonText: "Télécharger mon CV",
     cvButtonView: "Voir le CV",
     
@@ -41,13 +34,6 @@ const text = {
     sectionIntroText: "Born in Portugal, I am passionate about web development, both front-end and back-end. And no, not all geeks spend *only* their lives behind a screen... sometimes we touch grass too (real or in CSS) .",
     sectionSportTitle: "Sports & Karate",
     sectionSportText: `I love sports very much, which allowed me to have a career in karate. My whole life revolves around this sport: competitions, classes given to children, children's coaching... Here are some notable results:\r\n- 2020: French Championship — 3rd place\r\n- 2021: International Competition in Portugal — 1st place\r\n- 2021: International Competition in Luxembourg — 1st place\r\n- 2022: International Competition in Belgium — 1st place`,
-    sectionCinemaTitle: "Cinema",
-    sectionCinemaText: "Subsequently, I am passionate about cinema. I find that it is a real pleasure to go to the cinema to see good films which, in addition to enriching us culturally, make us aware of many things.",
-    sectionEsportTitle: "Esport & Video Games",
-    sectionEsportText: "Video games also mattered a lot during my youth, where I was concentrated only on sports. Coming home, my sources of motivation were games. I therefore joined Esport teams which gave me unique experiences. I think having participated in Karate competitions and Esport teams allowed me to improve my soft skills, notably communication, teamwork and stress management.",
-    sectionCuisineTitle: "Cooking",
-    sectionCuisineText: "What I especially love about cooking is the creative aspect, of being able to combine different ingredients and spices to arrive at a unique dish. It's a bit like web development, where you combine different elements to arrive at a functional and pleasant website to use.",
-    sectionMotivationText: "My portfolio is the fruit of creativity, but above all of hard work. I am always looking to learn, face new challenges and create modern and pleasant web experiences for those who visit my sites.",
     cvButtonText: "Download my CV",
     cvButtonView: "View CV",
     
@@ -69,13 +55,6 @@ const text = {
     sectionIntroText: "Nascido em Portugal, sou um entusiasta do desenvolvimento web, tanto front-end quanto back-end. E não, nem todos os geeks passam *apenas* a vida atrás de um ecrã... às vezes também tocamos relva (real ou em CSS) .",
     sectionSportTitle: "Desporto & Karaté",
     sectionSportText: `Adoro muito desporto, o que me permitiu ter uma carreira no karaté. Toda a minha vida gira em torno deste desporto: competições, aulas dadas a crianças, treino de crianças... Aqui estão alguns resultados notáveis:\r\n- 2020: Campeonato Francês — 3º lugar\r\n- 2021: Competição Internacional em Portugal — 1º lugar\r\n- 2021: Competição Internacional no Luxemburgo — 1º lugar\r\n- 2022: Competição Internacional na Bélgica — 1º lugar`,
-    sectionCinemaTitle: "Cinema",
-    sectionCinemaText: "Posteriormente, sou apaixonado por cinema. Acho que é um verdadeiro prazer ir ao cinema ver bons filmes que, além de nos enriquecerem culturalmente, nos fazem tomar consciência de muitas coisas.",
-    sectionEsportTitle: "Esport & Videojogos",
-    sectionEsportText: "Os videojogos também foram muito importantes durante a minha juventude, onde estava concentrado apenas no desporto. Ao chegar a casa, as minhas fontes de motivação eram os jogos. Por isso, integrei equipas de Esport que me deram experiências únicas. Penso que ter participado em competições de Karaté e em equipas de Esport me permitiu melhorar as minhas soft skills, nomeadamente a comunicação, o trabalho em equipa e a gestão do stress.",
-    sectionCuisineTitle: "Culinária",
-    sectionCuisineText: "O que mais gosto na culinária é o aspeto criativo, de poder combinar diferentes ingredientes e especiarias para chegar a um prato único. É um pouco como o desenvolvimento web, onde combinamos diferentes elementos para chegar a um site funcional e agradável de usar.",
-    sectionMotivationText: "O meu portfólio é fruto de criatividade, mas principalmente de muito trabalho. Estou sempre em busca de aprender, enfrentar novos desafios e criar experiências web modernas e agradáveis para quem visita os meus sites.",
     cvButtonText: "Baixar meu CV",
     cvButtonView: "Ver CV",
     
@@ -97,13 +76,6 @@ const text = {
     sectionIntroText: "Nacido en Portugal, soy un entusiasta del desarrollo web, tanto front-end como back-end. Y no, no todos los geeks pasan *solo* la vida detrás de una pantalla... a veces también tocamos hierba (real o en CSS) .",
     sectionSportTitle: "Deporte & Karate",
     sectionSportText: `Me encanta mucho el deporte, lo que me permitió tener una carrera en karate. Toda mi vida gira en torno a este deporte: competiciones, clases impartidas a niños, entrenamiento de niños... Aquí hay algunos resultados notables:\r\n- 2020: Campeonato de Francia — 3er lugar\r\n- 2021: Competencia Internacional en Portugal — 1er lugar\r\n- 2021: Competencia Internacional en Luxemburgo — 1er lugar\r\n- 2022: Competencia Internacional en Bélgica — 1er lugar`,
-    sectionCinemaTitle: "Cine",
-    sectionCinemaText: "Posteriormente, soy apasionado por el cine. Creo que es un verdadero placer ir al cine a ver buenas películas que, además de enriquecernos culturalmente, nos hacen tomar conciencia de muchas cosas.",
-    sectionEsportTitle: "Esports & Videojuegos",
-    sectionEsportText: "Los videojuegos también fueron muy importantes durante mi juventud, cuando estaba concentrado solo en el deporte. Al llegar a casa, mis fuentes de motivación eran los juegos. Por eso, me uní a equipos de Esport que me dieron experiencias únicas. Creo que haber participado en competencias de Karate y en equipos de Esport me permitió mejorar mis habilidades blandas, en particular la comunicación, el trabajo en equipo y la gestión del estrés.",
-    sectionCuisineTitle: "Cocina",
-    sectionCuisineText: "Lo que más me gusta de la cocina es el aspecto creativo, de poder combinar diferentes ingredientes y especias para llegar a un plato único. Es un poco como el desarrollo web, donde combinamos diferentes elementos para llegar a un sitio web funcional y agradable de usar.",
-    sectionMotivationText: "Mi portafolio es fruto de la creatividad, pero principalmente del trabajo duro. Siempre estoy buscando aprender, enfrentar nuevos desafíos y crear experiencias web modernas y agradables para quienes visitan mis sitios.",
     cvButtonText: "Descargar mi CV",
     cvButtonView: "Ver CV",
     
@@ -121,13 +93,7 @@ function applyText(lang) {
   document.querySelector('#section-intro p').textContent = t.sectionIntroText;
   document.querySelector('#section-sport h3').textContent = t.sectionSportTitle;
   document.querySelector('#section-sport p').textContent = t.sectionSportText;
-  document.querySelector('#section-cinema h3').textContent = t.sectionCinemaTitle;
-  document.querySelector('#section-cinema p').textContent = t.sectionCinemaText;
-  document.querySelector('#section-esport h3').textContent = t.sectionEsportTitle;
-  document.querySelector('#section-esport p').textContent = t.sectionEsportText;
-  document.querySelector('#section-cuisine h3').textContent = t.sectionCuisineTitle;
-  document.querySelector('#section-cuisine p').textContent = t.sectionCuisineText;
-  document.querySelector('#section-motivation p').textContent = t.sectionMotivationText;
+
 
   // Menu Hamburger & FOOTER
   const menuKeys = ['navAccueil','navProjets','navContact','navApropos','navInfo','navCV', 'theme', 'allRightsReserved', 'footerMentions', 'cvButtonText','cvButtonView'];
